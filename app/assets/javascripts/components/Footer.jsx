cddd0e20016ae2f30eb0655fbff49a741ab61452
Daniel <danielnguyensr@gmail.com>
2017-03-24 17:57:28 -0700

    Add pages controller, views, routes, components

---
+++ app/assets/javascripts/components/Footer.jsx
@@ -0,0 +1,5 @@
+class Footer extends React.Component {
+    render() {
+        return <h1>Hello Footer</h1>
+    }
+}
---
+++ app/assets/javascripts/components/Nav.jsx
@@ -0,0 +1,5 @@
+class Nav extends React.Component {
+    render() {
+        return <h1>Hello World</h1>
+    }
+}
